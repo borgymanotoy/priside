a50bae7699b1a0bd78260630690cadb1b1715ac0
Borgy Manotoy <ejsalipahmad@yahoo.com>
2014-11-27 23:46:30 +0800

    Removed old files

--- scripts/show_contacts.sql
+++
@@ -1,6 +0,0 @@
-create view show_contacts as 
-select 
-    concat(first_name, ' ', IF(LENGTH(substring(middle_name, 1, 1)) > 0, concat(substring(middle_name, 1, 1), '. '), ' '), last_name) as name,
-    email_add1,
-    contact_no
-from user_profile;
